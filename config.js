7f6ba06f371cbac0fac78eab055f1f8d939cace0
H <87992324+hashxxx@users.noreply.github.com>
2021-08-31 20:03:52 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+/*
+ __    __     _     _                 _     __                _           
+ / / /\ \ \___| |__ | |__   ___   ___ | | __/ _\ ___ _ __   __| | ___ _ __ 
+ \ \/  \/ / _ \ '_ \| '_ \ / _ \ / _ \| |/ /\ \ / _ \ '_ \ / _` |/ _ \ '__|
+  \  /\  /  __/ |_) | | | | (_) | (_) |   < _\ \  __/ | | | (_| |  __/ |   
+   \/  \/ \___|_.__/|_| |_|\___/ \___/|_|\_\\__/\___|_| |_|\__,_|\___|_|   
+     
+   Author: H
+   Discord: https://discord.gg/56qJcHxXCa
+   Github: 
+
+    ⚠️  < This is the only file you need to modify >
+ */
+
+// 1- Webhook link (url):
+exports.WEBHOOK = "";
+// 2- Bot profile picture (url):
+exports.PP = "https://cdn.discordapp.com/attachments/880171442564628542/880948724191354920/Flag.png";
+// 3- Bot username:
+exports.USERNAME = "Webhook";
+// 4- Message:
+exports.MESSAGE = "Webhook message test";
